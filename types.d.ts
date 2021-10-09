4e776c25a630b058561905bb82d28b35ea79e1f9
waddah-jd <waddah.ja@gmail.com>
2021-10-09 22:07:28 +0300

    add basic types for retrier param validators

---
+++ types.d.ts
@@ -0,0 +1,21 @@
+interface IntervalRetrierOptions {
+  times: number;
+  interval: number;
+}
+
+interface TimeoutRetrierOptions {
+  interval: number;
+  timeout: number;
+}
+
+interface ValidateFixedTimesRetrierOptionsParameter {
+  times: number;
+  interval?: number;
+}
+
+interface ValidateTimeoutRetrierOptionsParameter {
+  timeout: number;
+  interval?: number;
+}
+
+type RetrierOptions = IntervalRetrierOptions | TimeoutRetrierOptions;
