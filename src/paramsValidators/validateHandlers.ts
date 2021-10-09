2f16fc4e80d3645111a2d1706c7f8e39694c9372
waddah-jd <waddah.ja@gmail.com>
2021-10-09 22:36:28 +0300

    add basic handlers validators

---
+++ src/paramsValidators/validateHandlers.ts
@@ -0,0 +1,36 @@
+const validateHandlers = (handlers?: {
+  onSuccess?: CallbackHandler;
+  onFailure?: CallbackHandler;
+}): { onSuccess: CallbackHandler; onFailure: CallbackHandler } => {
+  const emptyFunction = () => undefined;
+
+  if (!handlers) {
+    return {
+      onSuccess: emptyFunction,
+      onFailure: emptyFunction,
+    };
+  }
+
+  if (handlers.onSuccess) {
+    if (typeof handlers.onSuccess !== "function") {
+      throw new Error(
+        `'onSuccess' handler must be a function instead it is of type: ${typeof handlers.onSuccess}`
+      );
+    }
+  }
+
+  if (handlers.onFailure) {
+    if (typeof handlers.onFailure !== "function") {
+      throw new Error(
+        `'onFailure' handler must be a function instead it is of type: ${typeof handlers.onFailure}`
+      );
+    }
+  }
+
+  return {
+    onSuccess: handlers.onSuccess || emptyFunction,
+    onFailure: handlers.onFailure || emptyFunction,
+  };
+};
+
+export default validateHandlers;
